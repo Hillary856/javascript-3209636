3d5ac60c03489d31137da4d43ab117abd7ae8708
hazza <hillary3247@gmail.com>
2025-10-21 15:24:15 -0500

    Add files via upload

--- 1-escenario-interactivo/app.js
+++ 1-escenario-interactivo/app.js
@@ -1,2 +1,23 @@
  const diamantes = document.querySelectorAll('.diamond');
- const score = document.querySelector('contador');
+ const score = document.querySelector('.contador');
+ let i = 0
+
+ // no puedo dar nulo
+ console.log(diamantes);
+ console.log(score);
+
+ diamantes.forEach( item => {
+    
+
+item.addEventListener('click', () => {
+    item.classList.add('saltar');
+    i++
+    score.textContent = i
+})
+
+
+item.addEventListener( 'animationend', () => {
+item.computedStyleMap.display = "none"
+}, {once: true})
+
+ })

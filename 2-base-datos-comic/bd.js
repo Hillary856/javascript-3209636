a819c2e174b3fdfcae805ddd6de5feca24422037
hazza <hillary3247@gmail.com>
2025-11-06 15:20:03 -0500

    Add files via upload

---
+++ 2-base-datos-comic/bd.js
@@ -0,0 +1,94 @@
+let comic = 
+{
+  "nombreComic": "Adrian y las cronicas del imperio",
+  "sipnosis": "Donde un arqueologo va investega la cueva de sus sueñosn adentrandose en un viaje un poco inesperado",
+  "year": 2025,
+  "autores": ["Hillary", "Andres", "Katherine", "Stiven"],
+  "personajes": ["Adrian", "Ninove", "Senador","Socrates", "Aristoteles", "Platon"],
+  "genero": "Ficcion",
+  "portada": "img/portada",
+
+
+  "escenas": [
+    {
+      "id": 1,
+      "name": "Cueva",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"],
+      "image": "Dan Jukes",
+
+    },
+    {
+     
+      "id": 2,
+      "name": "Molecule Mn",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"],
+      "image": "Dan Jukes",
+    },
+    {
+      
+
+      "id": 3,
+      "name": "Molecule Man",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"],
+      "image": "Dan Jukes",
+    },
+
+
+ {
+      
+
+      "id": 4,
+      "name": "Molecule Man",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"],
+      "image": "Dan Jukes",
+    },
+
+
+     {
+      
+
+      "id": 5,
+      "name": "Molecule Man",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"],
+      "image": "Dan Jukes",
+    },
+
+
+
+     {
+      
+
+      "id": 6,
+      "name": "Molecule Man",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"],
+      "image": "Dan Jukes",
+    },
+
+
+
+
+ {
+      
+
+      "id": 7,
+      "name": "Molecule Man",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"],
+      "image": "Dan Jukes",
+    },
+
+
+
+     {
+      
+
+      "id": 8,
+      "name": "Molecule Man",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"],
+      "image": "Dan Jukes",
+    },
+
+
+
+
+  ]
+}
